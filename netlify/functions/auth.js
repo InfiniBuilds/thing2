656a4248815a84b2bf28b5f637a2a1120f7f0c39
Infini_Builds <swaroop11.ds@gmail.com>
2025-12-23 12:26:56 +0530

    Delete netlify/functions/auth.js

--- netlify/functions/auth.js
+++
@@ -1,7 +0,0 @@
-const { NetlifyOAuth } = require('netlify-oauth-provider');
-
-exports.handler = NetlifyOAuth({
-  provider: 'github',
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-});
